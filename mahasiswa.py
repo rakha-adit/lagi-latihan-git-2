6fd673959de2631754ca04dc80118600b1bf3e3d
rakha-adit <rakhaaditya@protonmail.com>
2024-09-19 15:04:23 +0700

    change fill in mahasiswa.py

--- mahasiswa.py
+++ mahasiswa.py
@@ -4,11 +4,11 @@ class Mahasiswa:
         self.nim = nim
 
 
-print('COBA GIT')
+print('DATA')
 print('Data Mahasiswa')
-mahasiswa_1 = Mahasiswa('Ucok', 1234567890)
 mahasiswa_2 = Mahasiswa('Udin', 9087654321)
 mahasiswa_3 = Mahasiswa('Amanda', 1111111111)
-print(f'{mahasiswa_1.nama} : {mahasiswa_1.nim}')
 print(f'{mahasiswa_2.nama} : {mahasiswa_2.nim}')
 print(f'{mahasiswa_3.nama} : {mahasiswa_3.nim}')
+
+print("Hayoloh Ini Beda Paragraph")

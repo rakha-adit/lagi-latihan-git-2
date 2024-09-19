f14b7f52e4737c17e99019627bb23b065daeb631
rakha-adit <rakhaaditya@protonmail.com>
2024-09-19 15:01:31 +0700

    Change coba git to data mahasiswa, change data nim in mahasiswa and add contoh paragraph

--- mahasiswa.py
+++ mahasiswa.py
@@ -4,11 +4,13 @@ class Mahasiswa:
         self.nim = nim
 
 
-print('COBA GIT')
 print('Data Mahasiswa')
-mahasiswa_1 = Mahasiswa('Ucok', 1234567890)
-mahasiswa_2 = Mahasiswa('Udin', 9087654321)
-mahasiswa_3 = Mahasiswa('Amanda', 1111111111)
+print('Data Mahasiswa')
+mahasiswa_1 = Mahasiswa('Ucok', 5678)
+mahasiswa_2 = Mahasiswa('Udin', 9762)
+mahasiswa_3 = Mahasiswa('Amanda', 1234)
 print(f'{mahasiswa_1.nama} : {mahasiswa_1.nim}')
 print(f'{mahasiswa_2.nama} : {mahasiswa_2.nim}')
 print(f'{mahasiswa_3.nama} : {mahasiswa_3.nim}')
+
+print("Contoh Paragraf")
